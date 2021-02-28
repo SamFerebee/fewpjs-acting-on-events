94a484fcf6cda83bae4c489a05d8a4e4a8352ec5
Sam Ferebee <sam.ferebee@gmail.com>
2021-02-28 18:40:05 -0500

    commit tree 0fd544a85278ada5e60675a720854d18b8b0643b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    console.log(leftNumbers);
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let rightNums = dodger.style.left.replace("px", "")
+    let right = parseInt(rightNums, 10);
+    console.log(right);
+    if (right < 360)
+        dodger.style.left = `${right + 1}px`;
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
+
